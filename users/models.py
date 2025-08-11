01f99895c1c423eb625862d267e02a6a2138816b
shreekanth <shree.n.k585@gmail.com>
2025-08-12 01:59:27 +0530

    adding migrations file to project.

--- users/models.py
+++ users/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
-from .managers import CustomUserManager
+from users.managers import CustomUserManager
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(_("email address"), unique=True)
